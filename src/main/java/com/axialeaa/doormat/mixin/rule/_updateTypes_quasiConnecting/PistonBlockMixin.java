ab800efccfd709e0a67e4865a9b086e6cebb1d45
Axia <mcvoidredstone@gmail.com>
2023-10-28 03:08:14 +0100

    renamed a method

--- src/main/java/com/axialeaa/doormat/mixin/rule/_updateTypes_quasiConnecting/PistonBlockMixin.java
+++ src/main/java/com/axialeaa/doormat/mixin/rule/_updateTypes_quasiConnecting/PistonBlockMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.Slice;
 public class PistonBlockMixin {
 
     @ModifyArg(method = "onSyncedBlockEvent", slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/block/PistonBlock;move(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/Direction;Z)Z")), at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"))
-    private int disableQuasiConnecting(int flags) {
+    private int changeNeighborUpdate(int flags) {
         return DoormatSettings.pistonUpdateType.getFlags() | Block.MOVED;
     }
 
